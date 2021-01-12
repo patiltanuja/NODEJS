9fc6755d824291e32eab556c77293cfdfd3509fc
patiltanuja <72022066+patiltanuja@users.noreply.github.com>
2021-01-12 18:37:00 +0530

    Add files via upload

---
+++ patternDemo03.js
@@ -0,0 +1,62 @@
+//reactangular box
+// let num=process.argv[2];
+//  num=parseInt(num);
+// for (var i = 1; i <= num; i++) {
+//     let st = ""
+//     for (var j = 1; j <= num; j++) {
+//         st += "*";
+//     }
+//     console.log(st);
+// }
+
+// //number piryamid
+// let num=process.argv[2];
+//  num=parseInt(num);
+// for (var i = 1; i <= num; i++) {
+//     let st = ""
+//     for (var j = 1; j <= i; j++) {
+//         st += i;
+//     }
+//     console.log(st);
+// }
+
+//inverted pyrammid
+
+for (var i = 1; i <= 5; i++) {
+    let st = ""
+    for (var j = 5; j >= i; j--) {
+        st += "*";
+    }
+    console.log(st);
+}
+
+
+
+
+
+
+// //inverted pyramid
+// for (var i = 1; i <= 5; i++) {
+//     let st = ""
+//     for (var j = 5; j >= i; j--) {
+//         st += "*";
+    
+//     console.log(st);
+
+//     for (let j = i; j <= 5; j++) {
+//         st += "*";
+//     }
+//     console.log(st);
+// }
+// }
+
+
+// //pyramid
+// for (let i = 0; i < 5; i++) {
+//     let star = ""
+//     for (let j = 0; j <= i; j++) {
+//         star += "*";
+//     }
+//     console.log(star);
+// }
+
